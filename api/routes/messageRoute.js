a22014a64421ad791712fd18cb33af955e348d09
Barış Özgür Yaşar <yasarbarisozgur@gmail.com>
2024-02-20 21:23:21 +0300

    fiverApp message things
    fiverrApp doing conversation and message in users

--- api/routes/messageRoute.js
+++ api/routes/messageRoute.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import { verifyToken } from '../middleware/jwt.js';
+import {
+  createMessage,
+  getMessages,
+} from '../controllers/messageController.js';
 
 const router = express.Router();
 
 //creating first endpoint
-router.get('/test');
+router.post('/', verifyToken, createMessage);
+router.get('/:id', verifyToken, getMessages);
 
 export default router;
